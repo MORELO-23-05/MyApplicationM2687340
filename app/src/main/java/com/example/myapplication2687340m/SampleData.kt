af7740c04cee6f721ed10b1fb070c9fc101462d8
MORELO-23-05 <mariomorelo456@gmail.com>
2023-08-14 13:44:54 -0500

    Ejercicios Kotlin

---
+++ app/src/main/java/com/example/myapplication2687340m/SampleData.kt
@@ -0,0 +1,62 @@
+package com.example.myapplication2687340m
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Mario",
+            "Hola a panas ¿vamos a ver Fnaf " +
+                    "dicen que esta re buena y es sangrienta" +
+                    "ademas tengo 4 entradas" +
+                    "quieren ir?"
+        ),
+        Message(
+            "Jhoan",
+            "Claro bro \n"
+        ),
+        Message(
+            "Jaykel",
+            "No, mi pinchecha no me deja\n"
+        ),
+        Message(
+            "Ronald",
+            "Si ah bueno"
+        ),
+        Message(
+            "Jaykel",
+            "Es que la pinchecha no deja salir al pinchipe"
+        ),
+        Message(
+            "Ronald",
+            "¿entonces a donde vamos?"
+        ),
+        Message(
+            "Jhoan",
+            "yo digo que a jugar XboX ¿no se que piesan?"
+        ),
+        Message(
+            "Ronald",
+            "a mi me parece muy bien ^_^"
+        ),
+        Message(
+            "Jaykel",
+            "Pero mi novia no me dejan ir"
+        ),
+        Message(
+            "Mario",
+            "Y eso a mi que"
+        ),
+        Message(
+            "Ronald",
+            "si ah bueno, entonces quedamos asi"
+        ),
+        Message(
+            "Jhoam",
+            "listo"
+        ),
+        Message(
+            "Mario",
+            "listo"
+        ),
+    )
+}
